b1711a39ec8723e323879533b44e2e97376e0706
Corey Jones <cor@cmac-a.attlocal.net>
2018-09-04 18:47:59 -0500

    init file

---
+++ flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    #create and configure app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        #load the instance config, if it exists
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #load test config 
+        app.config.from_mapping(test_config)
+    
+    #ensure insance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    # a simple page to say hello
+    @app.route('/hello')
+    def hello():
+        return 'hello again, ugh'
+    return app
